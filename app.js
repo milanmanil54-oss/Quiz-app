aff2cd8ad7a802c41c30356d50e5776e8ee91368
milan m anil <milanmanil54@gmail.com>
2026-02-14 15:28:25 +0530

    js modied,almost done

--- app.js
+++ app.js
@@ -70,10 +70,10 @@ const questions = [
 ];
 
 const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-button");
 const nextButton = document.getElementById("next-btn");
 
-let cuurentQuestionIndex = 0;
+let currentQuestionIndex = 0;
 let score = 0;
 
 function startQuiz(){
@@ -84,7 +84,51 @@ function startQuiz(){
 }
 
 function showQuestion(){
+  resetState();
   let currentQuestion = questions[currentQuestionIndex];
   let questionNo = currentQuestionIndex + 1;
-  questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+currentQuestion.answers.forEach(answer => {
+  const button = document.createElement("button");
+  button.innerHTML = answer.text;
+  button.classList.add("btn");
+  answerButtons.appendChild(button);
+  if(answer.correct){
+    button.dataset.correct = answer.correct;
+  }
+
+  button.addEventListener("click",selectAnswer)
+
+});
+
+
 }
+
+function resetState(){
+  nextButton.style.display = "none"
+  while(answerButtons.firstChild)
+    answerButtons.removeChild(answerButtons.firstChild);
+
+}
+function selectAnswer(e){
+const selectedBtn = e.target;
+const isCorrect = selectedBtn.dataset.correct === "true";
+if(isCorrect){
+  selectedBtn.classList.add("correct");
+}
+else{
+  selectedBtn.classList.add("incorrect");
+}
+Array.from(answerButtons.children).forEach(button => {
+  if(button.dataset.correct === "true"){
+    button.classList.add("correct");
+  }
+  button.disabled = true;
+});
+nextButton.style.display = "block";
+}
+startQuiz();
+
+
+
